e20b5589ee541a2dc1594807cfcb6611af400b68
Patricio Pérez Pinto <patodeath@gmail.com>
2024-12-11 14:34:22 -0300

    fix: fix polybar bug

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-polybar-msg cmd quit
-echo "---" | tee -a /tmp/polybar1.log
-polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
-echo "Bars launched..."
+# Terminar todas las instancias existentes de Polybar
+killall -q polybar
+
+# Esperar a que los procesos se detengan
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Lanzar las barras
+polybar topbar &
+polybar bottombar &
